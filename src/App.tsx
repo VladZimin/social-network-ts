e4aff356cff813227cfe3977b6c86e31572d247c
VladZimin <vlad.zimin1@gmail.com>
2022-05-28 12:34:23 +0500

    lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
-import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import React from 'react'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-     <Technologies/>
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://img.icons8.com/ios/452/duolingo-logo.png' alt='Logo' />
+      </header>
+      <nav className='nav'>
+        <div>Profile</div>
+        <div>Profile</div>
+        <div>Profile</div>
+        <div>Profile</div>
+        <div>Profile</div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='http://severnykavkaz.ru/wp-content/uploads/2019/04/priroda-adygei-1200x540.jpg'
+               alt='Profile Image' />
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My Posts
+          <div>
+            New Posts
+          </div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
