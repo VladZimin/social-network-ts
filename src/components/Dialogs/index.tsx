daab4843e0a0d6dc7490d55c68a7ca8f58c44759
VladZimin <vlad.zimin1@gmail.com>
2022-06-08 12:47:51 +0500

    lesson 21

--- src/components/Dialogs/index.tsx
+++ src/components/Dialogs/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import s from './Dialogs.module.css'
+
+const usersList = [ 'Ivan', 'Vlad', 'Denis', 'Viktor', 'Vadim', 'Kolya' ]
 
 const Dialogs = () => {
-    return <div>
-        Dialogs
+    return <div className={s.dialogsWrapper}>
+        <div className={s.dialogsList}>
+            {usersList.map( ( user, i ) => <div className={s.userItem + ` ${i === 3 && s.activeDialog}`}>{user}</div> )}
+        </div>
+        <div className={s.messageBlock}>
+            <div className={s.messageItem}>Hello !</div>
+            <div className={s.messageItem}>How are you!</div>
+            <div className={s.messageItem}>Fine!</div>
+        </div>
     </div>
 }
 
